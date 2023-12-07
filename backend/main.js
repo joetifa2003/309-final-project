4108d74b678c04bb1d65279f5e680628e45c0c3c
Omar Goda <80228400+omargoda99@users.noreply.github.com>
2023-12-07 08:49:36 -0500

    Update main.js

--- backend/main.js
+++ backend/main.js
@@ -1,10 +1,22 @@
 const express = require("express");
 const products = require("./products/products");
+const mongoose = require("mongoose");
 
 const app = express();
 const port = 3000;
 
 app.use(express.json());
+
+mongoose.connect('mongodb://localhost:27017', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => {
+  console.log('MongoDB connected successfully');
+})
+.catch((err) => {
+  console.error('MongoDB connection error:', err);
+});
 
 app.use("/products", products);
 
