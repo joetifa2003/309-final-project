ff3b37892de2d201e9808c51de98438eeba7daa1
Omar Goda <80228400+omargoda99@users.noreply.github.com>
2023-12-07 08:11:59 -0500

    Create orderSchema.js

---
+++ backend/db/orderSchema.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const orderSchema = new Schema({
+  userID: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  productID: {
+    type: Schema.Types.ObjectId,
+    ref: 'Product',
+    required: true
+  }
+});
+
+const Order = mongoose.model('Order', orderSchema);
