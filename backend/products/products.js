79eea9bdfaf02e9f8d0658c00616d852ed405ca4
moustafahany <moustafahany@gmail.com>
2023-12-07 18:12:08 +0200

    Test

--- backend/products/products.js
+++ backend/products/products.js
@@ -7,5 +7,6 @@ router.get("/", async (req, res) => { });
 router.get("/:id", async (req, res) => { });
 router.post("/add", async (req, res) => { });
 router.delete("/delete", async (req, res) => { });
+console.log("Moustafa");
 
 module.exports = router;
