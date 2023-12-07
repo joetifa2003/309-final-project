6d13b508136487666b84042a934c7e182c6ac6d5
Omar Goda <80228400+omargoda99@users.noreply.github.com>
2023-12-07 14:48:54 -0500

    Update cart.js

--- backend/routers/cart/cart.js
+++ backend/routers/cart/cart.js
@@ -1,9 +1,15 @@
 const express=require("express");
-const { authenticated}=require("../auth/auth");
-const {addTOCart}=require("../../db/cart");
+const {authenticated}=require("../auth/auth");
+const { addToCart, removeFromCart } = require("../../db/cart");
 const router=express.Router();
 router.post("/add",authenticated,async (req,res)=>{
     const userID=req.user.id;
     const{productID}=req.body;
     await addTOCart(userID,productID);
-})
+});
+router.delete("/remove", authenticated, async (req, res) => {
+    const userID = req.user.id;
+    const {productID}=req.body;
+    await removeFromCart(userID,productID);
+});
+module.exports = router;
