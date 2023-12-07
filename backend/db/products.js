20acb2885fd2250f4df0d3e16643aad02837c913
Youssef ahmed <youssef.cyza@gmail.com>
2023-12-07 23:10:29 +0200

    feat: add get product by id

--- backend/db/products.js
+++ backend/db/products.js
@@ -4,6 +4,11 @@ async function getAllProducts() {
   return await Product.find();
 }
 
+async function getProductById(id) {
+  return await Product.findById(id);
+}
+
 module.exports = {
   getAllProducts,
+  getProductById,
 };
