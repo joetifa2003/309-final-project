d60896c7f6ee943516747a8a23e34d0c5a2b5518
Youssef ahmed <youssef.cyza@gmail.com>
2023-12-07 19:06:28 +0200

    add product by ID query

--- backend/products/products.js
+++ backend/products/products.js
@@ -1,14 +1,15 @@
 const express = require("express");
-const { getAllProducts } = require("../db/products");
+const { getAllProducts, getProductById } = require("../db/products");
 
 const router = express.Router();
 
 router.get("/", async (req, res) => {
-    let products =await getAllProducts();
+    let products = await getAllProducts();
     res.json(products);
- });
+});
 
 router.get("/:id", async (req, res) => { });
+
 router.post("/add", async (req, res) => { });
 router.delete("/delete", async (req, res) => { });
 
