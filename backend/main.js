c2ee71088571610cb909235fbd79054eae9492c9
Youssef ahmed <youssef.cyza@gmail.com>
2023-12-08 00:44:55 +0200

    feat: add cors

--- backend/main.js
+++ backend/main.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
+
 const products = require("./routers/products/products");
 const cart = require("./routers/cart/cart");
 const { router: auth } = require("./routers/auth/auth");
@@ -8,6 +10,7 @@ const app = express();
 const port = 2000;
 
 app.use(express.json());
+app.use(cors());
 
 mongoose.set("strictQuery", false);
 mongoose
