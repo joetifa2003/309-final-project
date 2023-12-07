74243b28bd738d4a30cefb8b24f97e1d112c70ae
Youssef ahmed <youssef.cyza@gmail.com>
2023-12-08 01:47:24 +0200

    feat: navigate to home page after sigup/login

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,9 +1,11 @@
 import { useCallback, useContext } from "react";
+import { useNavigate } from "react-router-dom";
 import { UserContext } from "../context/user";
 import api from "../lib/axios";
 
 function Login() {
   const { setToken } = useContext(UserContext);
+  const nav = useNavigate();
 
   const onSubmit = useCallback(async (event) => {
     event.preventDefault();
@@ -19,6 +21,7 @@ function Login() {
       const token = res.data.token;
       localStorage.setItem("token", token);
       setToken(token);
+      nav("/");
     } catch (e) {
       alert("invalid email or password");
     }
--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,9 +1,11 @@
 import { useCallback, useContext } from "react";
+import { useNavigate } from "react-router-dom";
 import { UserContext } from "../context/user";
 import api from "../lib/axios";
 
 function SignUp() {
   const { setToken } = useContext(UserContext);
+  const nav = useNavigate();
 
   const onSubmit = useCallback(async (event) => {
     event.preventDefault();
@@ -26,6 +28,7 @@ function SignUp() {
       const token = res.data.token;
       localStorage.setItem("token", token);
       setToken(token);
+      nav("/");
     } catch (e) {
       alert("an account already exists with that email");
     }
