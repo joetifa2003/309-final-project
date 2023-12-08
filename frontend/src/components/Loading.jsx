80ba3de1ede555e8aef41eb176e2a45726a99173
Youssef ahmed <youssef.cyza@gmail.com>
2023-12-08 15:53:30 +0200

    add loading indicator

--- frontend/src/components/Loading.jsx
+++ frontend/src/components/Loading.jsx
@@ -1,1 +1,13 @@
-export const Loading = () => <span>Loading...</span>;
+import { Bars } from "react-loader-spinner";
+
+export const Loading = () => (
+  <div className="fixed right-0 top-0 flex h-full w-full items-center justify-center bg-white">
+    <Bars
+      height="80"
+      width="80"
+      color="#EEC39E"
+      ariaLabel="bars-loading"
+      visible={true}
+    />
+  </div>
+);
