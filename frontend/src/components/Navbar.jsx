eebcad34d26ca57a951e355558b0fc5d81b6b026
Youssef ahmed <youssef.cyza@gmail.com>
2023-12-08 15:33:05 +0200

    feat: responsive nav

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,16 +1,18 @@
-import { useCallback, useContext } from "react";
+import Hamburger from "hamburger-react";
+import { useCallback, useContext, useState } from "react";
 import { Link } from "react-router-dom";
-import AvatarIcon from "../assets/avatar.svg";
 import CartIcon from "../assets/cart.svg";
 import { UserContext } from "../context/user";
 
 function NavBar() {
   const { user, setToken, setUser } = useContext(UserContext);
+  const [menuOpened, setMenuOpend] = useState(false);
 
   const logOut = useCallback(() => {
     localStorage.removeItem("token");
     setToken(null);
     setUser(null);
+    setMenuOpend(false);
   }, []);
 
   return (
@@ -21,11 +23,10 @@ function NavBar() {
             Furniture
           </Link>
         </div>
-        <div className="flex items-center space-x-6">
+        <div className="hidden items-center space-x-6 md:flex">
           <Link to="/user/cart">
             <img className="h-8 w-8 cursor-pointer" src={CartIcon} />
           </Link>
-          <img className="h-8 w-8 cursor-pointer" src={AvatarIcon} />
           {!user ? (
             <>
               <Link to={"/signup"} className="btn btn-small">
@@ -41,6 +42,54 @@ function NavBar() {
             </button>
           )}
         </div>
+        <span className="md:hidden">
+          <Hamburger
+            toggled={menuOpened}
+            toggle={setMenuOpend}
+            className="md:hidden"
+          />
+        </span>
+        {menuOpened && (
+          <>
+            <div
+              className="fixed right-0 top-0 h-full w-full bg-black bg-opacity-50"
+              onClick={() => setMenuOpend(false)}
+            ></div>
+            <div className="fixed right-0 top-0 h-full w-3/4 bg-light-gray">
+              {!user ? (
+                <div className="flex flex-col space-y-4">
+                  <Link
+                    to={"/signup"}
+                    className="btn btn-small bg-white"
+                    onClick={() => setMenuOpend(false)}
+                  >
+                    Sign up
+                  </Link>
+                  <Link
+                    to={"/login"}
+                    className="btn btn-small bg-white"
+                    onClick={() => setMenuOpend(false)}
+                  >
+                    Login
+                  </Link>
+                </div>
+              ) : (
+                <div className="flex flex-col space-y-4">
+                  <Link
+                    to="/user/cart"
+                    className="btn btn-small bg-white"
+                    onClick={() => setMenuOpend(false)}
+                  >
+                    Cart
+                  </Link>
+                  <button onClick={logOut} className="btn btn-small bg-white">
+                    Log out
+                  </button>
+                </div>
+              )}
+            </div>
+          </>
+        )}
       </div>
     </nav>
   );
